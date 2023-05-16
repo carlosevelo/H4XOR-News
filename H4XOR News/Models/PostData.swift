a0fd55c703004b148f30f3a3e7035a2b095f8860
Carlos Evelo <carlosevelo96@gmail.com>
2023-05-16 13:47:08 -0600

    Whole app

---
+++ H4XOR News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Carlos Evelo on 5/16/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
